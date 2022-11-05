43eb177a449925884a4c9289f38ec41668dc7bcc
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-05 16:47:10 +0600

    Created types.d.ts file

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+export interface IItem {
+  name: string;
+  price: number;
+  img: string;
+  count: number;
+}
+
+export interface IItemData {
+  name: string;
+  price: number;
+  count: number;
+}
