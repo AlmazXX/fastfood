53d6d124d7b5eb5c9ee52a6dc78fd8281f1f612f
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-05 16:49:25 +0600

    Created Item component

---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Item.css";
+
+interface Props {
+  name: string;
+  price: number;
+  img: string;
+  addItem: (name: string) => void;
+}
+
+const Item: React.FC<Props> = ({ name, price, img, addItem }) => {
+  return (
+    <div className="item" onClick={() => addItem(name)}>
+      <div>
+        <img src={img} alt={name} />
+      </div>
+      <div>
+        <p>{name}</p>
+        <span>Price: {price} som</span>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
