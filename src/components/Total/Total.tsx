f4c81488b538061350f4e4a0f14b955d2cc9a281
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-05 16:55:07 +0600

    Added Total component for counting total price

---
+++ src/components/Total/Total.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Total.css';
+
+interface Props {total : number}
+
+const Total: React.FC<Props> = ({total}) => {
+  return (
+    <p className='total'>
+        Total: <span>{total} KGS</span>
+      </p>
+  );
+};
+
+export default Total;
